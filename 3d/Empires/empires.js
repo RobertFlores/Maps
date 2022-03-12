57ba0970b001084b699cf5e6972e17a422203214
Robert Flores <rflores99z@outlook.com>
2022-03-12 12:18:44 -0800

    Added 3d maps - Empires & Acts

---
+++ 3d/Empires/empires.js
@@ -0,0 +1,11 @@
+// var layers = [];
+// var buttons = [];
+
+function swapLayers (index) {
+    // for (i = 0; i < viewer.dataSources.length; i++){
+    //   viewer.dataSources.get(i).show = false;
+    //   if (index == i) {
+    //     viewer.dataSources.get(i).show = true;
+    //   }
+    // }
+}
